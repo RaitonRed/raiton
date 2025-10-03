4823da3f1086f873b4b8171f740516b209dcb6ea
Minton <89341024+RaitonRed@users.noreply.github.com>
2025-10-03 02:24:44 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "Raiton",
-	subtitle: "place for reading",
+	subtitle: "Teenager and his daily life",
 	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -48,11 +48,11 @@ export const navBarConfig: NavBarConfig = {
 			url: "https://github.com/RaitonRed", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
-		{
+		/* {
 			name: "Blog",
 			url: "https://raitonlog.ir",
 			external: true, // Show an external link icon and will open in a new tab
-		},
+		}, */
 	],
 };
 
